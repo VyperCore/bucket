2b6483c6be98a2e57056a5300f77e5252a62e18d
Edward Kotarski <edktrsk@gmail.com>
2024-01-04 11:19:23 +0000

    Some docs

--- fcov/common/chain.py
+++ fcov/common/chain.py
@@ -1,3 +1,32 @@
+"""
+This file contains utilities to label the bounds of items in a tree structure. 
+
+Each node is given start and end bounds such that a child's start bound is
+always >= a parents start bound, a child's end bound is always <= to a 
+parents end bound, and a node's end bound is always > its start bound.
+
+This is done by (starting at the root node):
+  1. Get this node start bound from one of:
+    - (For root node) an initial value, usually 0.
+    - (for the first child of a parent) the start bound of the parent .
+    -  (for subsequent children) the end bound of the the previous child.
+  2. Recurse 1.
+  3. Add the size of this node (usually 1) to create the end bound of this 
+     node.
+
+We describe this process as chaining through the tree, with each Link being
+connected to a node in the tree, and containing the start and end bound 
+information. Links are opened from parents or siblings and passed through
+to the next node.
+
+When a link is closed, it adds itself to an shared index, which each link in
+a chain has a reference to. The index can be used to iterate over links of 
+a certain type in a chain.
+
+The start and end bounds can be complex multi-faceted values so long as they
+can be added together.
+"""
+
 from collections import defaultdict
 from typing import Any, Hashable, Optional, Generic, Protocol, Self, TypeVar
 
