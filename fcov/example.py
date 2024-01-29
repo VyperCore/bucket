82cfb61ae8eb0fd119f115a7c0eb7800c3677ea7
Stuart Alldred <stuartalldred@gmail.com>
2024-01-29 22:26:31 +0000

    Updated example

--- fcov/example.py
+++ fcov/example.py
@@ -9,116 +9,199 @@ from .sampler import Sampler
 
 from .rw import PointReader, SQLAccessor, MergeReading, ConsoleWriter
 
-# TODO
-# - dump coverage at end of test into YAML
-# - register triggers (default every clock cycle)
-# - register cvg tier: only instance when required
-# - present YAML in usable way
-# - Trace struct, with setting functions
-# - Optimise code
+# This file contains useful reference code (such as example coverpoints,
+# covergroups, etc), as well as some necessary code to get the example working. 
 
+# Covergroups
+class TopDogs(Covergroup):
+    def setup(self, ctx):
+        self.add_coverpoint(DogStats(name="doggy_stats", description="Some basic doggy stats"))
+        self.add_covergroup(
+            DogsAndToys(name="doggy_coverage", description="A group of coverpoints about dogs")
+        )
 
-# def floatTrigger(trace):
-#     return True if trace else false
+class DogsAndToys(Covergroup):
+    def setup(self, ctx):
+        self.add_coverpoint(ChewToysByAge(name="chew_toys_by_age", description="Preferred chew toys by age"))
+        self.add_coverpoint(
+            ChewToysByName(name="chew_toys_by_name__group_a", description="Preferred chew toys by name - Group A", names=["Barbara", "Connie", "Graham"])
+        )
+        self.add_coverpoint(
+            ChewToysByName(name="chew_toys_by_name__group_b", description="Preferred chew toys by name - Group B", names=["Clive", "Derek", "Ethel"])
+        )
 
-
-# coverage
-class MyCoverpoint(Coverpoint):
-    def __init__(self, name: str, description: str, trigger=None):
-        super().__init__(name, description, trigger=None)
+class DogStats(Coverpoint):
+    def __init__(self, name: str, description: str):
+        super().__init__(name, description)
 
     def setup(self, ctx):
         self.add_axis(
-            name="my_axis_1",
-            values=[0, 1, [2, 3]],
-            description="Range of values for my_axis_1",
+            name="name",
+            values=["Clive", "Derek", "Ethel", "Barbara", "Connie", "Graham"],
+            description="All the acceptable dog names",
         )
         self.add_axis(
-            name="my_axis_2",
-            values=[6, 7, 8, 9],
-            description="Range of values for my_axis_2",
+            name="age",
+            values=list(range(16)),
+            description="Dog age in years",
         )
         self.add_axis(
-            name="my_axis_3",
-            values={"red": 0, "blue": 1, "green": [2, 3], "yellow": 4},
-            description="Range of values for my_axis_3",
+            name="size",
+            values={"Small": [0, 10], "medium": [11, 30], "large": [31, 50]},
+            description="Rough size estimate from weight",
         )
 
-        self.add_goal("MOULDY_CHEESE", -1, "Not so gouda!")
-        self.add_goal("OPTMISTIC_CHEESE", 20, "I brie-live in myself!")
+        self.add_goal("HECKIN_CHONKY", -1, "Puppies can't be this big!")
 
     def apply_goals(self, bucket, goals):
-        if bucket.my_axis_1 == "1" and bucket.my_axis_3 in ["red", "yellow"]:
-            return goals.MOULDY_CHEESE
-        elif int(bucket.my_axis_2) > 8:
-            return goals.OPTMISTIC_CHEESE
+        if int(bucket.age) <= 1 and bucket.size in ["large"]:
+            return goals.HECKIN_CHONKY
 
     def sample(self, trace):
         # 'with cursor' is used, so cursor values are cleared each time
         # cursor can also be manaually cleared by using cursor.clear()
         with self.cursor as cursor:
             cursor.set_cursor(
-                my_axis_1=trace[0],
-                my_axis_2=trace[1],
+                name=trace['Name'],
+                age=trace['Age'],
+                size=trace['Weight']
+            )
+            self.cursor.increment()
+
+class ChewToysByAge(Coverpoint):
+    def __init__(self, name: str, description: str):
+        super().__init__(name, description)
+
+    def setup(self, ctx):
+
+        self.add_axis(
+            name="breed",
+            values={"Border Collie":[0,1], "Whippet":2, "Labrador":3, "Cockapoo":4},
+            description="All known dog breeds"
+        )
+        self.add_axis(
+            name="age",
+            values=['Puppy', 'Adult', 'Senior'],
+            description="Range of dog years",
+        )
+        self.add_axis(
+            name="favourite_toy",
+            values=["Slipper", "Ball", "Stick", "Ring"],
+            description="Types of dog toys",
+        )
+
+        self.add_goal("NO_SLIPPERS", -1, "Only puppies chew slippers!")
+        self.add_goal("STICK", 50, "Yay sticks!")
+
+    def apply_goals(self, bucket, goals):
+        if bucket.age != "Puppy" and bucket.favourite_toy in ["slipper"]:
+            return goals.NO_SLIPPERS
+        elif bucket.favourite_toy == "stick":
+            return goals.STICK
+
+    def sample(self, trace):
+        # 'with cursor' is used, so cursor values are cleared each time
+        # cursor can also be manaually cleared by using cursor.clear()
+
+        dog_age = trace['Age']
+        if dog_age < 2:
+            age = 'Puppy'
+        elif dog_age > 12:
+            age = 'Senior'
+        else:
+            age = 'Adult'
+
+        with self.cursor as cursor:
+            cursor.set_cursor(
+                breed=trace['Breed'],
+                age=age,
             )
 
             # For when multiple values might need covering from one trace
             # Only need to set the axes that change
-            for colour in range(len(trace[2])):
-                cursor.set_cursor(my_axis_3=trace[2][colour])
+            for toy in range(len(trace['Chew_toy'])):
+                cursor.set_cursor(favourite_toy=trace['Chew_toy'][toy])
                 cursor.increment()
 
 
-# covergroups
-class MyCovergroup(Covergroup):
+
+class ChewToysByName(Coverpoint):
+    def __init__(self, name: str, description: str, names):
+        self.name_group = names
+        super().__init__(name, description)
+
     def setup(self, ctx):
-        self.add_coverpoint(MyCoverpoint(name="my_coverpoint", description="A lovely coverpoint"))
-        self.add_coverpoint(
-            MyCoverpoint(name="another_coverpoint", description="A rather spiffing coverpoint")
+        self.add_axis(
+            name="breed",
+            values={"Border Collie":[0,1], "Whippet":2, "Labrador":3, "Cockapoo":4},
+            description="All known dog breeds"
+        )
+        self.add_axis(
+            name="name",
+            values=self.name_group,
+            description="Most important dog names only",
+        )
+        self.add_axis(
+            name="favourite_toy",
+            values=["Slipper", "Ball", "Stick", "Ring"],
+            description="Types of dog toys",
         )
 
+    def sample(self, trace):
+        # 'with cursor' is used, so cursor values are cleared each time
+        # cursor can also be manaually cleared by using cursor.clear()
 
-class MyBigCoverGroup(Covergroup):
-    def setup(self, ctx):
-        self.add_covergroup(
-            MyCovergroup(name="my_covergroup", description="A group of coverpoints")
-        )
-        self.add_coverpoint(MyCoverpoint(name="some_coverpoint", description="A meh coverpoint"))
+        if trace['Name'] not in self.name_group:
+            return
+        
+        with self.cursor as cursor:
+            cursor.set_cursor(
+                breed=trace['Breed'],
+                name=trace['Name'],
+            )
+
+            # For when multiple values might need covering from one trace
+            # Only need to set the axes that change
+            for toy in range(len(trace['Chew_toy'])):
+                cursor.set_cursor(favourite_toy=trace['Chew_toy'][toy])
+                cursor.increment()
 
 
 # Sampler
 class MySampler(Sampler):
     def __init__(self, coverage):
         super().__init__(coverage=coverage)
-
+        
         import random
-
-        self.random = random.Random(2023)
-
+        self.random = random.Random()
 
     def create_trace(self):
         """Nonsense function"""
         # This should come from monitors, etc
-        trace_data_1 = self.random.randint(0, 3)
-        trace_data_2 = self.random.randint(6, 9)
-        trace_data_3 = self.random.choices(["red", "green", "yellow", "blue", 0, 1, 2, 3, 4], k=2)
+        # For this example, create random doggy data and put in a dictionary
+        # In practise this would normally be a class, which can be populated with many
+        # types of data as required.
+        trace = {}
+        trace['Breed'] = self.random.choice(["Border Collie", "Whippet", "Labrador", "Cockapoo", 0, 1, 2, 3, 4])
+        trace['Chew_toy'] = self.random.choices(["Slipper", "Ball", "Stick", "Ring"], k=2)
+        trace['Weight'] = self.random.randint(5, 50)
+        trace['Age'] = self.random.randint(0, 15)
+        trace['Name'] = self.random.choice(["Clive", "Derek", "Ethel", "Barbara", "Connie", "Graham"])
 
-        data = (trace_data_1, trace_data_2, trace_data_3)
-        print(f"TRACE_DATA: {data}")
-        return data
+        return trace
 
 
 
 if __name__ == "__main__":
     # testbench
     with CoverageContext(isa="THIS IS AN ISA"):
-        cvg_a = MyBigCoverGroup(name="my_big_covergroup", description="A group of stuff")
+        cvg_a = TopDogs(name="Dogs", description="Doggy coverage")
 
     with CoverageContext(isa="THIS IS AN ISA"):
-        cvg_b = MyBigCoverGroup(name="my_big_covergroup", description="A group of stuff")
+        cvg_b = TopDogs(name="Dogs", description="Doggy coverage")
 
     cvg_a.print_tree()
-    cvg_a.my_covergroup.print_tree()
+    cvg_a.doggy_coverage.print_tree()
 
     sampler = MySampler(coverage=cvg_a)
     for _ in range(100):
@@ -155,10 +238,13 @@ if __name__ == "__main__":
     merged_reading = MergeReading(sql_reading_a, sql_reading_b)
 
     # Output to console
+    print("This is the coverage with 100 samples:")
     ConsoleWriter(axes=False, goals=False, points=False).write(reading_a)
+    print("This is the coverage from 2 regressions. One with 100 samples, and one with 500:")
     ConsoleWriter(axes=False, goals=False, points=False).write(merged_reading)
 
     # Read all back from sql - note as the db is not removed this will 
     # acumulate each time this example is run.
     merged_reading_all = MergeReading(*sql_accessor.read_all())
+    print("This is the coverage from all regressions run so far:")
     ConsoleWriter(axes=False, goals=False, points=False).write(merged_reading_all)
