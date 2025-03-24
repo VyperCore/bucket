253f1f3c6a96741fc7b5da7720b723b0088792d7
Stuart Alldred <stuartalldred@gmail.com>
2025-03-24 09:18:25 +0000

    Add more pytests for Axis (#74)
    Replace asserts with ensure
    Update tests to check for assertion class, rather than match strings
    
    New tests for remaining Axis functions (with the exception of chain - to
    be tested at a higher level)

--- tests/test_axis.py
+++ tests/test_axis.py
@@ -3,7 +3,15 @@
 
 import pytest
 
-from bucket.axis import Axis
+from bucket.axis import (
+    Axis,
+    AxisIncorrectNameFormat,
+    AxisIncorrectValueFormat,
+    AxisOtherNameAlreadyInUse,
+    AxisRangeIncorrectLength,
+    AxisRangeNotInt,
+    AxisUnrecognisedValue,
+)
 
 
 class TestSanitiseValues:
@@ -105,7 +113,7 @@ class TestSanitiseValues:
             "Banana": 2,
             "Apple": 1,
         }
-        with pytest.raises(AssertionError, match='Values already contains "Apple"'):
+        with pytest.raises(AxisOtherNameAlreadyInUse):
             axis.sanitise_values(test_stimulus)
 
     def test_named_other_non_string(self):
@@ -129,16 +137,10 @@ class TestSanitiseValues:
         Pass in a set range with too few values
         Should raise an assertion error.
         """
-        axis = Axis(
-            name="test",
-            values=[0],
-            description="test",
-        )
+        axis = Axis(name="test", values=[0], description="test")
 
         test_stimulus = [1, 2, 3, 4, {5}]
-        with pytest.raises(
-            AssertionError, match="length of range is not 2. Length was 1"
-        ):
+        with pytest.raises(AxisRangeIncorrectLength):
             axis.sanitise_values(test_stimulus)
 
     def test_oversized_list_range(self):
@@ -146,16 +148,10 @@ class TestSanitiseValues:
         Pass in a list range with too many values
         Should raise an assertion error.
         """
-        axis = Axis(
-            name="test",
-            values=[0],
-            description="test",
-        )
+        axis = Axis(name="test", values=[0], description="test")
 
         test_stimulus = [1, 2, 3, 4, [5, 6, 7]]
-        with pytest.raises(
-            AssertionError, match="length of range is not 2. Length was 3"
-        ):
+        with pytest.raises(AxisRangeIncorrectLength):
             axis.sanitise_values(test_stimulus)
 
     def test_non_int_tuple_range(self):
@@ -163,16 +159,10 @@ class TestSanitiseValues:
         Pass in a tuple range with non-ints
         Should raise an assertion error.
         """
-        axis = Axis(
-            name="test",
-            values=[0],
-            description="test",
-        )
+        axis = Axis(name="test", values=[0], description="test")
 
         test_stimulus = [1, 2, 3, 4, ("Steve", "Bob")]
-        with pytest.raises(
-            AssertionError, match="Ranges should be specified as integers"
-        ):
+        with pytest.raises(AxisRangeNotInt):
             axis.sanitise_values(test_stimulus)
 
     def test_dict_with_undersized_range(self):
@@ -180,16 +170,10 @@ class TestSanitiseValues:
         Pass in a dict with an undersized range
         Should raise an assertion error.
         """
-        axis = Axis(
-            name="test",
-            values=[0],
-            description="test",
-        )
+        axis = Axis(name="test", values=[0], description="test")
 
         test_stimulus = {"Apple": 1, "Banana": 2, "Cherry": [3]}
-        with pytest.raises(
-            AssertionError, match="length of range is not 2. Length was 1"
-        ):
+        with pytest.raises(AxisRangeIncorrectLength):
             axis.sanitise_values(test_stimulus)
 
     def test_dict_with_oversized_range(self):
@@ -197,16 +181,10 @@ class TestSanitiseValues:
         Pass in a dict with an oversized range
         Should raise an assertion error.
         """
-        axis = Axis(
-            name="test",
-            values=[0],
-            description="test",
-        )
+        axis = Axis(name="test", values=[0], description="test")
 
         test_stimulus = {"Apple": 1, "Banana": 2, "Cherry": [50, 63, 75, 88]}
-        with pytest.raises(
-            AssertionError, match="length of range is not 2. Length was 4"
-        ):
+        with pytest.raises(AxisRangeIncorrectLength):
             axis.sanitise_values(test_stimulus)
 
     def test_dict_with_non_int_range(self):
@@ -214,16 +192,10 @@ class TestSanitiseValues:
         Pass in a tuple range with non-ints, as part of dict
         Should raise an assertion error.
         """
-        axis = Axis(
-            name="test",
-            values=[0],
-            description="test",
-        )
+        axis = Axis(name="test", values=[0], description="test")
 
         test_stimulus = {"Apple": 1, "Banana": 2, "Cherry": ("Steve", "Bob")}
-        with pytest.raises(
-            AssertionError, match="Ranges should be specified as integers"
-        ):
+        with pytest.raises(AxisRangeNotInt):
             axis.sanitise_values(test_stimulus)
 
     def test_set_range_values(self):
@@ -263,8 +235,159 @@ class TestSanitiseValues:
             1: 1,
         }
 
-        with pytest.raises(
-            AssertionError,
-            match="Values provided for axis are incorrectly formatted: 3 is int. All names must be string",
-        ):
+        with pytest.raises(AxisIncorrectNameFormat):
             axis.sanitise_values(test_stimulus)
+
+    def test_incorrect_values_type(self):
+        """Check that incorrect type for values is not allowed"""
+        axis = Axis(name="test", values=[0], description="test")
+
+        test_stimulus = 1
+
+        with pytest.raises(AxisIncorrectValueFormat):
+            axis.sanitise_values(test_stimulus)
+
+
+class TestGetNamedValue:
+    def test_unrecognised_value(self):
+        """Check unrecognised value raises an exception"""
+        axis = Axis(name="test", values=[1, 2, 4, 5], description="test")
+
+        test_stimuli = [0, 3, "3", 99, "Steve"]
+
+        for test_stimulus in test_stimuli:
+            with pytest.raises(AxisUnrecognisedValue):
+                axis.get_named_value(test_stimulus)
+
+    def test_unrecognised_value_with_range(self):
+        """Check unrecognised value with ranges raises an exception"""
+        axis = Axis(name="test", values=[[1, 3], [5, 7]], description="test")
+
+        test_stimuli = [0, "2", 4, "4", 99, "Steve"]
+
+        for test_stimulus in test_stimuli:
+            with pytest.raises(AxisUnrecognisedValue):
+                axis.get_named_value(test_stimulus)
+
+    def test_string_value_with_name(self):
+        """Check that a string name of the value is returned"""
+        axis = Axis(name="test", values=[1, 2, 3, [4, 7]], description="test")
+
+        test_stimuli = {1: "1", "1": "1", 4: "4 -> 7", 5: "4 -> 7", "4 -> 7": "4 -> 7"}
+        for test_stimulus, expected_result in test_stimuli.items():
+            assert axis.get_named_value(test_stimulus) == expected_result
+
+    def test_string_value_with_name_with_other(self):
+        """Check that a string name of the value is returned"""
+        axis = Axis(
+            name="test", values=[1, 2, 3, [4, 7]], description="test", enable_other=True
+        )
+
+        test_stimuli = {
+            1: "1",
+            "1": "1",
+            4: "4 -> 7",
+            8: "Other",
+            "7": "Other",
+            "7 -> 8": "Other",
+            "Steve": "Other",
+        }
+        for test_stimulus, expected_result in test_stimuli.items():
+            assert axis.get_named_value(test_stimulus) == expected_result
+
+    def test_string_value_with_name_with_named_other(self):
+        """Check that a string name of the value is returned"""
+        axis = Axis(
+            name="test",
+            values=[1, 2, 3, [4, 7]],
+            description="test",
+            enable_other="Weird",
+        )
+
+        test_stimuli = {
+            1: "1",
+            "1": "1",
+            4: "4 -> 7",
+            8: "Weird",
+            "7": "Weird",
+            "7 -> 8": "Weird",
+            "Steve": "Weird",
+        }
+        for test_stimulus, expected_result in test_stimuli.items():
+            assert axis.get_named_value(test_stimulus) == expected_result
+
+    def test_named_dict(self):
+        """Check that a string name of the value is returned"""
+        axis = Axis(
+            name="test", values={"ten": 10, "less_than_ten": [0, 9]}, description="test"
+        )
+
+        test_stimuli = {
+            10: "ten",
+            "ten": "ten",
+            4: "less_than_ten",
+            0: "less_than_ten",
+            9: "less_than_ten",
+            "less_than_ten": "less_than_ten",
+        }
+        for test_stimulus, expected_result in test_stimuli.items():
+            assert axis.get_named_value(test_stimulus) == expected_result
+
+    def test_named_dict_with_other(self):
+        """Check that a string name of the value is returned"""
+        axis = Axis(
+            name="test",
+            values={"ten": 10, "less_than_ten": [0, 9]},
+            description="test",
+            enable_other=True,
+        )
+
+        test_stimuli = {
+            10: "ten",
+            "ten": "ten",
+            4: "less_than_ten",
+            "10": "Other",
+            11: "Other",
+            "Steve": "Other",
+        }
+        for test_stimulus, expected_result in test_stimuli.items():
+            assert axis.get_named_value(test_stimulus) == expected_result
+
+    def test_named_dict_with_named_other(self):
+        """Check that a string name of the value is returned"""
+        axis = Axis(
+            name="test",
+            values={"ten": 10, "less_than_ten": [0, 9]},
+            description="test",
+            enable_other="Weird",
+        )
+
+        test_stimuli = {
+            10: "ten",
+            "ten": "ten",
+            4: "less_than_ten",
+            "10": "Weird",
+            11: "Weird",
+            "Steve": "Weird",
+        }
+        for test_stimulus, expected_result in test_stimuli.items():
+            assert axis.get_named_value(test_stimulus) == expected_result
+
+    def test_cached_values(self):
+        """Check that when the same value is checked, the result is cached correctly"""
+        axis = Axis(
+            name="test", values=[1, 2, 3, [4, 5]], description="test", enable_other=True
+        )
+
+        test_stimuli = [1, 4, 5, 6]
+        for count, test_stimulus in enumerate(test_stimuli, start=1):
+            result_1 = axis.get_named_value(test_stimulus)
+            result_2 = axis.get_named_value(test_stimulus)
+
+            assert result_1 == result_2
+
+            # Verify that a cache hit occurred
+            cache_info = axis.get_named_value.cache_info()
+            # The first call doesn't count as a hit,
+            # but the second call should be a hit.
+            assert cache_info.hits == count
